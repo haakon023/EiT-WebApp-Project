a8f6b74b8fc1ef31c46459e6d75d2e432fab9ec0
lukashg <lukashg@stud.ntnu.no>
2022-03-30 10:42:58 +0200

    Added text to information and contact components

--- src/components/ContactComponent.js
+++ src/components/ContactComponent.js
@@ -5,7 +5,11 @@ class ContactComponent extends React.Component
     render(){
         return (
             <main style={{ padding: "1rem 0" }}>
-                <h2>Contacts: gjengen!</h2>
+                <div className="information-text">
+                    <h2>
+                        Contact Team Trojansk Test. Will probably not respond. 
+                    </h2>
+                </div>
             </main>
         );
     }
